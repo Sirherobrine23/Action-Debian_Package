348793ecb6a789cd9cd2b625a92edc1eb17670d7
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-12-22 14:51:25 -0300

    Update repo.js

--- repo.js
+++ repo.js
@@ -5,7 +5,7 @@ const time = (new Date()).toTimeString();
 core.setOutput("time", time);
 // TIME
 // console.log(`Process ${process.cwd()},\n Dirname ${__dirname}\n\n\n`)
-var command = `cd ${__dirname}/src/ && chmod 777 repo.sh && ./repo.sh`
+var command = `chmod 777 ${__dirname}/src/repo.sh && ${__dirname}/src/repo.sh`
 var serverstated = exec(command, {
     detached: false,
     shell: true,
