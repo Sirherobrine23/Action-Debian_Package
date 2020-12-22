6fc5d38334e2d574f7b5d68bf0953a6fd39dd648
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-12-22 14:34:47 -0300

    push

--- src/setup.sh
+++ src/setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo $PWD
-echo 'chmod'
-chmod 777 *.sh
-echo 'running'
-./install.sh
+set -x
+DEBIAN_FRONTEND=noninteractive
+# 
+sudo apt update
+sudo apt install -y curl dos2unix wget git dpkg-dev
+sudo apt clean
+# 
+apt purge --remove *dotnet* -y
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /usr/local/lib/android
+# Autoremove
+sudo apt-get -qq autoremove --purge
+sudo swapoff -a
+sudo rm -rf /mnt/swap*
+exit 0
