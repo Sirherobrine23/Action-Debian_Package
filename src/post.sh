57d5ca3d0869d962379b0ea0a9582f4367d51eb6
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-12-22 17:33:25 -0300

    Update post.sh

--- src/post.sh
+++ src/post.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo "Preparing to upload the file"
-
-if [ $INPUT_GIT == 'true' ];then
+if [ $INPUT_DEBUG == 'true' ];then
     env
+fi
+echo "Preparing to upload the file"
+if [ $INPUT_GIT == 'true' ];then
     git config --global user.name ${GITHUB_ACTOR}
     git config --global user.email "actions@github.com"
     if [ $INPUT_URL == 'http://' ];then
@@ -15,9 +16,9 @@ if [ $INPUT_GIT == 'true' ];then
     echo "cloned the repository"
     git clone $REPO -b $INPUT_BRANCH --depth=1 /tmp/repo
     cd /tmp/repo/$INPUT_PATH
-    cp -rfv DEB_PATH ./
+    cp -rfv $DEB_PATH ./
     git add .
-    git commit -m "Package add from ${GITHUB_REPO}"
+    git commit -m "Package add from ${GITHUB_REPOSITORY}"
     git push
 fi
 #
